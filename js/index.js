9684d522bac06fbc29379005f43ec5e530b8272f
Ray Aguilar <ray.aguilar15@gmail.com>
2019-10-05 12:52:13 -0500

    mvp complete

--- js/index.js
+++ js/index.js
@@ -41,7 +41,7 @@ const siteContent = {
 
 //IMages
 let logo = document.getElementById("logo-img");
-logo.src = "img/logo.png";
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
 
 const mainImg = document.querySelector("#cta-img");
 mainImg.src = "img/header-img.png";
@@ -52,14 +52,19 @@ middleImg.src = "img/mid-page-accent.jpg";
 //Nav Bar
 
 const navBar = document.querySelectorAll("a");
-navBar[0].textContent = "Services";
-navBar[1].textContent = "Product";
-navBar[2].textContent = "Vision";
-navBar[3].textContent = "Features";
-navBar[4].textContent = "About";
-navBar[5].textContent = "Contact";
+// navBar[0].textContent = "Services";
+// navBar[1].textContent = "Product";
+// navBar[2].textContent = "Vision";
+// navBar[3].textContent = "Features";
+// navBar[4].textContent = "About";
+// navBar[5].textContent = "Contact";
 
-console.log(navBar[2].textContent); //Vision
+console.log(navBar); //Vision
+
+navBar.forEach((item, i) => {
+  item.textContent = siteContent.nav[`nav-item-${i++}`]
+
+})
 
 // "nav": {
 //   "nav-item-1": "Services",
